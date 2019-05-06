e1c533366bb71701d92b2e6a30f0297a0c983d68
Haripriyadarsini <harnie04@gmail.com>
2019-05-06 12:48:22 +0530

    First message

---
+++ CustomerInfo.java
@@ -0,0 +1,22 @@
+package com.greens.customer;//PACKAGE
+
+public class CustomerInfo //CLASS
+ {
+	public void customerId() //METHOD
+	{
+	System.out.println("CustomerID is 1");
+		}
+	public void customerName() //METHOD
+	{
+		System.out.println("CustomerName is XYZ");
+		
+	}
+
+	public static void main(String[] args) {
+//Create object		
+CustomerInfo x=new CustomerInfo();
+x.customerId();
+x.customerName();
+	}
+
+}
